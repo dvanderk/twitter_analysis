0f46d921f18b47bd05db8b719e56d86b2249e7c3
Diederique van der Knaap <dvanderknaap@me.com>
2016-12-21 14:24:30 -0500

    Updating freq

--- frequency.py
+++ frequency.py
@@ -8,38 +8,27 @@ import sys
 
 def main():
     tweet_file = open(sys.argv[1])
-
-    words = {} #dictionary to hold terms and their frequencies
-    tweet_text = [] #holds the texts of the tweets   
+    words = {} #dictionary to hold terms and their frequencies   
+    total = 0
 
     #seperate just the text of the tweets from erroneous data
     for line in tweet_file:
-	#print line
 	tweet = json.loads(line)
-	print tweet['text']
 	if 'text' in tweet:
-	    print tweet
-	    text = tweet['text'].lower()
-	    tweet_text.append(text.encode('utf-8'))
-    #print tweet_text
+	    text = tweet['text'].lower().encode('utf-8').split()
+	    #create the dictionary for each word, counting as we go
+	    for term in text:
+		if term in words:
+		    words[term] += 1
+		else:
+		    words[term] = 1
+	    total += 1
 
-    #create the dictionary for each word, counting as we go
-    total = 0    
-    for t in tweet_text:
-	for word in t:
-	    if word not in words:
-		words[word] = 1
-	    else:
-		words[word] += 1
-	total += 1
 
     #print out the terms and their frequencies
-    for term in words:
-	print term + " " + str(words[term]/total)
+    for term in sorted(words, key = words.get):
+	print term ," ", (words[term]/float(total))
 
-   # print total
-    #print words.keys()
-   # print "hello world"
    
 if __name__ == '__main__':
     main()
